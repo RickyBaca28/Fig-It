84cb70678661bd368ac098147e9d2c731b69fab8
RickyBaca28 <Rickybaca28@gmail.com>
2021-01-30 15:13:09 -0800

    feat: added in mongoose and created db file

---
+++ backend/src/db.ts
@@ -0,0 +1,38 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+class Database {
+    private db: Mongoose;
+
+    private dbUrl: string;
+
+    constructor(dbUrl: string ) {
+        this.dbUrl = dbUrl;
+
+        mongoose.set('useFindAndModify', false);
+        mongoose.set('useCreateIndex', true);
+        mongoose.set('useUnifiedTopology', true);
+        mongoose.set('useNewUrlParser', true);
+    }
+
+    async connect(): Promise<void> {
+        try {
+            console.log('Attempting to connect MongoDB!');
+            this.db = await mongoose.connect(this.dbUrl);
+        } catch (err) {
+            throw new Error('Unable to connect to mongo database, check to see if docker image is running');
+        }
+        console.log('Connected to MongoDB!');
+    }
+
+    async close(): Promise<void> {
+        try {
+            if(this.db) {
+                await this.db.disconnect();
+                console.log('Closed connection to MongoDB');
+            }
+        } catch (err) {
+            console.log(`Can't close connection to MongoDB`);
+        }
+    }
+}
+export default Database;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Fig-It's backend!`);
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end(`Fig-It's backend!`);
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const yargs = require('yargs/yargs')
+
+const { argv } = yargs.options({ 
+a: { alias: 'api-port', type: 'number', desc: 'Port to start the API server on' },
+d: { alias: 'db-url', type: 'string', desc: 'Mongo database url' },
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//
+// --- SET UP BACKEND ----------------------------------------------------------------
+//
+
+require('dotenv').config();
+
