f83cb132f225ab2921f7ebde1f57bcab61b6f450
RickyBaca28 <Rickybaca28@gmail.com>
2021-02-03 20:18:34 -0800

    feat: created user login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Authentication/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Login">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          You think it, we trade it!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fig-It
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Login/>
       </header>
     </div>
   );
